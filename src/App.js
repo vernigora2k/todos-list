79586edab4d650fbc83a371cc1f884d3de8cdd16
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-05-19 13:58:25 +0300

    v1.0.6stable 55:20 min finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import TodoList from './Todo/TodoList';
+import Context from './context';
+import AddTodo from './Todo/AddTodo';
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -8,23 +10,39 @@ function App() {
       {id: 3, completed: false, title: 'Купить молоко'}
     ])
 
-function toggleTodo(id) {
-  setTodos(
-    todos.map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo
-    })
-  )
-}
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false
+      }])
+    )
+  }
 
   return (
-    <div className="wrapper">
-      <h1>React tutorial</h1>
-
-      <TodoList todos={todos} onToggle={toggleTodo}></TodoList>
-    </div>
+    <Context.Provider value={{removeTodo}}>
+      <div className="wrapper">
+        <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo}></AddTodo>
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}></TodoList> : <p>There is no todos</p>}
+        
+      </div>
+    </Context.Provider>
   )
 }
 
--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useContext} from 'react'
 import PropTypes from 'prop-types'
+import Context from '../context'
 
 const styles = {
     li: {
@@ -17,10 +18,8 @@ const styles = {
 }
 
  function TodoItem({ todo, index, change }) {
-     console.log(todo)
+    const { removeTodo } = useContext(Context)
     const classes = []
-    console.log('classes is',classes)
-    console.log('todo.completed = ', todo.completed)
 
     if (todo.completed) {
         classes.push('done')
@@ -40,7 +39,7 @@ const styles = {
             {todo.title}
         </span>
 
-        <button className='rm'>&times;</button>
+        <button className='rm' onClick={() => removeTodo(todo.id)}>&times;</button>
     </li>
     )
 }
